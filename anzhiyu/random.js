d8cdb0059b2888448ed1bc7483c928e9f0922c06
lxy1234cn <a18865607558@qq.com>
2024-11-03 23:29:24 +0800

    Site updated: 2024-11-03 23:29:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/03/hello-world/"];function toRandomPost(){
+var posts=["2024/11/03/新文章/","2024/11/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
