ee59f3875f715ca133555b3a5def43a4a3a3c4c8
lxy1234cn <a18865607558@qq.com>
2024-11-04 12:33:10 +0800

    Site updated: 2024-11-04 12:33:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/03/新文章/","2024/11/03/hello-world/"];function toRandomPost(){
+var posts=["2024/11/03/新文章/","2024/11/04/基于C-的简单远程注册功能实现/","2024/11/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
